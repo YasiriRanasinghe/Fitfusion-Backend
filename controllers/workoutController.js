142c3f005c177ea635fe1dde4cbecdc7757e67da
Yasiri Ranasinghe <yasiridranasinghe@gmail.com>
2023-05-21 20:59:53 +0530

    Changes in routes

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,126 +1,68 @@
-const Workout = require("../models/workoutModel");
+const Workout = require('../models/workoutModel');
 
-const getWorkout = async (req, res) => {
+// Get all workouts
+const getAllWorkouts = async (req, res) => {
   try {
     const workouts = await Workout.find();
-    res.json(workouts);
+    res.status(200).json({ workouts });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while retrieving the workouts" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
+// Get a workout by ID
 const getWorkoutById = async (req, res) => {
   try {
     const workout = await Workout.findById(req.params.id);
     if (!workout) {
-      return res.status(404).json({ error: "Workout not found" });
+      return res.status(404).json({ error: 'Workout not found' });
     }
-
-    res.json(workout);
+    res.status(200).json({ workout });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while retrieving the workout" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
+// Create a new workout
 const createWorkout = async (req, res) => {
   try {
-    const workout = new Workout({
-      id: parseInt(req.body.id),
-      media_type: req.body.media_type,
-      original_name: req.body.original_name,
-      original_title: req.body.original_title,
-      poster_path: req.body.poster_path,
-      overview: req.body.overview,
-      vote_count: parseInt(req.body.vote_count),
-      release_date: req.body.release_date,
-      vote_average: parseInt(req.body.vote_average),
-      content: req.body.content,
-      createdAt: req.body.createdAt,
-    });
-
-    const savedWorkout = await workout.save();
-    res.status(201).json(savedWorkout);
+    const newWorkout = await Workout.create(req.body);
+    res.status(201).json({ workout: newWorkout });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while creating the workout" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
+// Update a workout
 const updateWorkout = async (req, res) => {
   try {
-    const workout = await Workout.findByIdAndUpdate(
-      req.params.id,
-      {
-        id: req.params.id,
-        media_type: req.body.media_type,
-        original_name: req.body.original_name,
-        original_title: req.body.original_title,
-        poster_path: req.body.poster_path,
-        overview: req.body.overview,
-        vote_count: req.params.vote_count,
-        release_date: req.body.release_date,
-        vote_average: req.params.vote_average,
-        content: req.body.content,
-        createdAt: req.body.createdAt,
-      },
-      { new: true }
-    );
-
+    const workout = await Workout.findByIdAndUpdate(req.params.id, req.body, { new: true });
     if (!workout) {
-      return res.status(404).json({ error: "Workout not found" });
+      return res.status(404).json({ error: 'Workout not found' });
     }
-
-    res.json(workout);
+    res.status(200).json({ workout });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while updating the workout" });
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
+// Delete a workout
 const deleteWorkout = async (req, res) => {
   try {
-    const workout = await Workout.findByIdAndRemove(req.params.id);
-
+    const workout = await Workout.findByIdAndDelete(req.params.id);
     if (!workout) {
-      return res.status(404).json({ error: "Workout not found" });
+      return res.status(404).json({ error: 'Workout not found' });
     }
-
-    res.json({ message: "Workout deleted successfully" });
+    res.status(200).json({ message: 'Workout deleted successfully' });
   } catch (error) {
-    res
-      .status(500)
-      .json({ error: "An error occurred while deleting the workout" });
-  }
-};
-
-const searchWorkout = async (req, res) => {
-  const { original_title } = req.body;
-
-  try {
-    const workouts = await Workout.find({ original_title: original_title });
-
-    if (!workouts || workouts.length === 0) {
-      return res.status(400).json({ error: 'No workouts were found' });
-    }
-
-    res.status(200).json(workouts);
-  } catch (error) {
-    console.error('Error searching workouts:', error);
-    res.status(500).json({ error: 'An error occurred while searching workouts' });
+    res.status(500).json({ error: 'Internal server error' });
   }
 };
 
 module.exports = {
-  getWorkout,
-  createWorkout,
+  getAllWorkouts,
   getWorkoutById,
+  createWorkout,
   updateWorkout,
   deleteWorkout,
-  searchWorkout
 };
--- routes/workoutRoute.js
+++ routes/workoutRoute.js
@@ -1,19 +1,31 @@
-const {
-  getWorkout,
-  getWorkoutById,
-  createWorkout,
-  updateWorkout,
-  deleteWorkout,
-  searchWorkout
-} = require("../controllers/workoutController");
-const router = require("express").Router();
+// const {
+//   getWorkout,
+//   getWorkoutById,
+//   createWorkout,
+//   updateWorkout,
+//   deleteWorkout,
+//   searchWorkout
+// } = require("../controllers/workoutController");
+// const router = require("express").Router();
 
-router.get("/", getWorkout);
-router.get("/:id", getWorkoutById);
-router.post("/", createWorkout);
-router.put("/:id", updateWorkout);
-router.delete("/:id", deleteWorkout);
-router.post('/search', searchWorkout);
+// router.get("/", getWorkout);
+// router.get("/:id", getWorkoutById);
+// router.post("/", createWorkout);
+// router.put("/:id", updateWorkout);
+// router.delete("/:id", deleteWorkout);
+// router.post('/search', searchWorkout);
+
+// module.exports = router;
+
+const express = require('express');
+const router = express.Router();
+const workoutController = require('../controllers/workoutController');
+
+// Define routes
+router.get('/workouts', workoutController.getAllWorkouts);
+router.get('/workouts/:id', workoutController.getWorkoutById);
+router.post('/workouts', workoutController.createWorkout);
+router.put('/workouts/:id', workoutController.updateWorkout);
+router.delete('/workouts/:id', workoutController.deleteWorkout);
 
 module.exports = router;
-
